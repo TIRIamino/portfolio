5285bf2875d9e973199af59865146e572926a864
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-02-14 17:08:08 +0100

    create pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'about',     to: 'pages#about',     as: :about
+  get 'contact',   to: 'pages#contact',   as: :contact
+  get 'portfolio', to: 'pages#portfolio', as: :portfolio
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
