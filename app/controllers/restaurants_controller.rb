1825edc2ec7314a7ef863d5a490a1a75ec7057ef
thierry vinagre <vinagre.thierry@yahoo.fr>
2020-02-15 10:37:31 +0100

    Portfolio with show, index, create

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,11 +2,28 @@ class RestaurantsController<ApplicationController
 
   RESTAURANTS = {
     1 => { name: "Dishoom", address: "Shoreditch, London", category: "indian" },
-    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" }
+    2 => { name: "Sushi Samba", address: "City, London", category: "japanese" },
+    3 => { name: "La Sauzé", address: "City, Belgarde", category: "french" },
+    4 => { name: "Chez Tao", address: "City, Marseille", category: "japanese" },
+    5 => { name: "Pendjab", address: "City, Lille", category: "indian" },
   }
 
   def index
-    @restaurants = RESTAURANTS
+    if params[:food_type].blank?
+      @restaurants = RESTAURANTS
+    else
+      @restaurants = RESTAURANTS.select do |id, restaurant|
+        restaurant[:category] == params[:food_type]
+      end
+    end
   end
 
+  def create
+    #TODO : create a Restautant in DB
+    render plain: "Add to DB restaurant '#{params[:name]}' with address '#{params[:address]}' and category '#{params[:category]}"
+  end
+
+  def show
+    @restaurant = RESTAURANTS[params[:id].to_i]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
-Rails.application.routes.draw do
-  get 'about',       to: 'pages#about',     as: :about
-  get 'contact',     to: 'pages#contact',   as: :contact
-  get 'team',   to: 'pages#team', as: :team
 
-  get 'restaurants', to: 'restaurants#index'
+Rails.application.routes.draw do
+  get 'about',            to: 'pages#about',     as: :about
+  get 'contact',          to: 'pages#contact',   as: :contact
+  get 'team',             to: 'pages#team',      as: :team
+
+  get 'restaurants',      to: 'restaurants#index'
+  post 'restaurants',     to: 'restaurants#create'
+  get 'restaurants/:id',  to: 'restaurants#show', as: :restaurant
+          #path
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
